61d51a4464338115b6b2f394669998326a4e5c2f
tanyangyang5 <937411899@qq.com>
2025-12-20 12:29:37 +0800

    Initial commit: add Spring Boot project

---
+++ src/test/java/com/example/aigenerate/AiGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aigenerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGenerateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
